37736de6a5342d080b64a37a3e4f6916e5be066e
PhuongNgan2103 <nguyenphuongngan2103@gmail.com>
2021-03-09 14:13:44 +0700

    th_ham

--- typescript-demo/src/functions.ts
+++ typescript-demo/src/functions.ts
@@ -5,12 +5,13 @@ function square(num: number): number {
 }
 
 console.log(square(5));
-// Output: 25
+
 
 // Function expression
 const squareFE = function (num: number): number {
   return num * num;
 }
+console.log(squareFE(9));
 
 // Higher-order function
 function add(a: number): Function {
@@ -22,4 +23,3 @@ function add(a: number): Function {
 const addWith5 = add(5);
 console.log(addWith5(3));
 console.log(addWith5(15));
-
